2f006e5a13f2f3a46044b5e5be92f1a7321194e0
AlaaMashour1 <148611610+AlaaMashour1@users.noreply.github.com>
2023-10-23 15:01:06 +0300

    Add files via upload

---
+++ keypad.h
@@ -0,0 +1,16 @@
+/*
+ * keypad.h
+ *
+ * Created: 10/21/23 2:30:31 PM
+ *  Author: Alaa
+ */ 
+
+
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+				
+void keypad_init (void);
+u8 keypad_getkey(void);
+
+#endif /* KEYPAD_H_ */
